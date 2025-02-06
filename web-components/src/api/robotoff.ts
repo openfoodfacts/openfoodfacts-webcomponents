176772237bc28660080be8bda3fa064710b81b5b
Kout95 <antoine.bernier@outlook.fr>
2025-02-06 18:11:27 +0100

    feat: WIP

--- web-components/src/api/robotoff.ts
+++ web-components/src/api/robotoff.ts
@@ -1,25 +1,27 @@
 import { ROBOTOFF_API_URL, IS_DEVELOPMENT_MODE } from "../constants"
 import { addParamsToUrl, removeEmptyKeys } from "../utils"
 import { getLocale } from "../localization"
-import { QuestionRequestParams, QuestionsResponse } from "../types/robotoff"
+import {
+  QuestionRequestParams,
+  QuestionsResponse,
+  QuestionAnnotationAnswer,
+} from "../types/robotoff"
 
 const robotoff = {
-  annotate(insightId: string, annotation: string) {
+  annotate(insightId: string, annotation: QuestionAnnotationAnswer) {
+    const body = JSON.stringify({
+      insight_id: insightId,
+      annotation,
+    })
     if (IS_DEVELOPMENT_MODE) {
-      console.log(
-        `Annotated, ${ROBOTOFF_API_URL}/insights/annotate`,
-        new URLSearchParams(
-          `insight_id=${insightId}&annotation=${annotation}&update=1`
-        )
-      )
+      console.log(`Annotated, ${ROBOTOFF_API_URL}/insights/annotate`, body)
       return
     } else {
       return fetch(`${ROBOTOFF_API_URL}/insights/annotate`, {
         method: "POST",
-        body: new URLSearchParams(
-          `insight_id=${insightId}&annotation=${annotation}&update=1`
-        ),
-        credentials: "include",
+        body,
+        // Is is necessary to include credentials?
+        // credentials: "include",
       })
     }
   },
@@ -38,190 +40,6 @@ const robotoff = {
     const response = await fetch(url)
     const result: QuestionsResponse = await response.json()
     return result
-  },
-  //
-  // async questions(filterState: QuestionRequestParams, count = 10, page = 1) {
-  //   const {
-  //     insightType,
-  //     brandFilter,
-  //     valueTag,
-  //     countryFilter,
-  //     sortByPopularity,
-  //     campaign,
-  //     predictor,
-  //   } = filterState
-  //
-  //   // const searchParams = {
-  //   //   insight_types: insightType,
-  //   //   value_tag: valueTag,
-  //   //   brands: reformatValueTag(brandFilter),
-  //   //   countries: countryId2countryCode(
-  //   //     countryFilter !== "en:world" ? countryFilter : null
-  //   //   ),
-  //   //   campaign,
-  //   //   predictor,
-  //   //   order_by: sortByPopularity ? "popularity" : "random",
-  //   // }
-  //
-  //   const lang = getLocale()
-  //
-  //   const response = await fetch(
-  //     `${ROBOTOFF_API_URL}/questions/?${new URLSearchParams(
-  //       removeEmptyKeys({
-  //         ...searchParams,
-  //         lang,
-  //         count,
-  //         page,
-  //       })
-  //     )}`
-  //   )
-  //   return response.json()
-  // },
-
-  async insightDetail(insight_id: string) {
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/insights/detail/${insight_id}`
-    )
-    return response.json()
-  },
-
-  async loadLogo(logoId: string) {
-    const response = await fetch(`${ROBOTOFF_API_URL}/images/logos/${logoId}`)
-    return response.json()
-  },
-
-  async updateLogo(logoId: string, value: any, type: string) {
-    return fetch(`${ROBOTOFF_API_URL}/images/logos/${logoId}`, {
-      method: "PUT",
-      body: JSON.stringify(removeEmptyKeys({ value, type })),
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-  },
-
-  async searchLogos(
-    barcode: string,
-    value: any,
-    type: string,
-    count = 25,
-    random = false
-  ) {
-    const formattedValue = /^[a-z][a-z]:/.test(value)
-      ? { taxonomy_value: value }
-      : { value }
-
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/images/logos/search/?${new URLSearchParams(
-        removeEmptyKeys({
-          barcode,
-          type,
-          count,
-          random,
-          ...formattedValue,
-        })
-      )}`
-    )
-    return response.json()
-  },
-
-  async getLogoAnnotations(logoId: string, index: number, count = 25) {
-    const url =
-      logoId.length > 0
-        ? `${ROBOTOFF_API_URL}/ann/search/${logoId}`
-        : `${ROBOTOFF_API_URL}/ann/search`
-    const response = await fetch(
-      `${url}?${new URLSearchParams(removeEmptyKeys({ index, count }))}`
-    )
-    return response.json()
-  },
-
-  async annotateLogos(annotations: string) {
-    return fetch(`${ROBOTOFF_API_URL}/images/logos/annotate`, {
-      method: "POST",
-      body: JSON.stringify(removeEmptyKeys({ annotations })),
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-  },
-
-  async getInsights(
-    barcode = "",
-    insightType = "",
-    valueTag = "",
-    annotation = "",
-    page = 1,
-    count = 25,
-    campaigns = "",
-    country = ""
-  ) {
-    let annotated
-    if (annotation.length && annotation === "not_annotated") {
-      annotated = "0"
-      annotation = ""
-    }
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/insights?${new URLSearchParams(
-        removeEmptyKeys({
-          barcode,
-          insight_types: insightType,
-          value_tag: valueTag,
-          annotation,
-          page,
-          annotated,
-          count,
-          campaigns,
-          countries: country,
-        })
-      )}`
-    )
-    return response.json()
-  },
-
-  async getUserStatistics(username: string) {
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/users/statistics/${username}`
-    )
-    return response.json()
-  },
-
-  getCroppedImageUrl(
-    imageUrl: string,
-    boundingBox: [number, number, number, number]
-  ) {
-    const [y_min, x_min, y_max, x_max] = boundingBox
-    return `${ROBOTOFF_API_URL}/images/crop?image_url=${imageUrl}&y_min=${y_min}&x_min=${x_min}&y_max=${y_max}&x_max=${x_max}`
-  },
-
-  async getLogosImages(logoIds: string[]) {
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/images/logos?logo_ids=${logoIds.join(",")}`
-    )
-    return response.json()
-  },
-
-  async getUnansweredValues(params: {
-    type: "label" | "brand" | "category"
-    countryCode: string
-    campaign: string
-    page?: number
-    count?: number
-  }) {
-    const { page = 1, countryCode, ...other } = params
-
-    const response = await fetch(
-      `${ROBOTOFF_API_URL}/questions/unanswered/?${new URLSearchParams(
-        removeEmptyKeys({
-          ...other,
-          countries: countryCode,
-          page: page >= 1 ? page : 1,
-        })
-      )}`
-    )
-    return response.json()
   },
 }
 
--- web-components/src/hunger/hunger-question-form.ts
+++ web-components/src/hunger/hunger-question-form.ts
@@ -1,6 +1,9 @@
 import { LitElement, html, css, nothing } from "lit"
 import { customElement, property } from "lit/decorators.js"
 import { Question } from "../types/robotoff"
+import robotoff from "../api/robotoff"
+import { nextQuestion } from "../signals/questions"
+import {localized, msg} from "@lit/localize";
 
 /**
  * An example element.
@@ -9,6 +12,7 @@ import { Question } from "../types/robotoff"
  * @slot - This element has a slot
  * @csspart button - The button
  */
+@localized()
 @customElement("hunger-question-form")
 export class HungerQuestionForm extends LitElement {
   static override styles = css`
@@ -26,12 +30,37 @@ export class HungerQuestionForm extends LitElement {
   @property({ type: Object, reflect: true })
   question?: Question
 
+  @property({ type: Boolean, attribute: "show-message" })
+  showMessage: boolean = false
+
+  @state()
+  private first?: boolean = true
+
+  private _annotateProduct = async (value: string) => {
+    robotoff.annotate(this.question!.insight_id, value)
+    nextQuestion()
+  }
+
+  private renderMessage() {
+    if (!this.showMessage) {
+      return nothing
+    } else if (this.first) {
+      this.first = false
+      return html`<div>${msg("Open foods facts need your help for this product")}<div>`
+    }
+
+    return html`<div>${msg("Thanks for your help, can you ")}</div>`
+
+  }
+
   override render() {
     if (!this.question) {
       return html`<div>No question</div>`
     }
+
     return html`
       <div>
+        ${this.renderMessage()}
         <h2>${this.question.question}</h2>
         ${this.question.source_image_url
           ? html`<img
@@ -42,9 +71,14 @@ export class HungerQuestionForm extends LitElement {
           : nothing}
         <div>
           <p>${this.question.value}</p>
-          <button>Yes</button>
-          <button>No</button>
-          <button>Skip</button>
+          <button @click="${this._annotateProduct" data-value="1">Yes</button>
+          <button @click="${this._annotateProduct}" data-value="0">No</button>
+          <button
+            @click="${this._annotateProduct}"
+            data-value="-1"
+          >
+            Skip
+          </button>
         </div>
       </div>
     `
