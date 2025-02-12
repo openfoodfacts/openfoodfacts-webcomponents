ad5cd6c15724d77dfcb749e2f7c7381bac957fd9
Kout95 <antoine.bernier@outlook.fr>
2025-02-12 15:08:14 +0100

    feat: rename hunger to robotoff

--- web-components/src/off-webcomponents.ts
+++ web-components/src/off-webcomponents.ts
@@ -1,6 +1,6 @@
 export { setLocale, getLocale } from "./localization"
-export { HungerQuestion } from "./components/robotoff/robotoff-question"
-export { HungerQuestionForm } from "./components/robotoff/robotoff-question-form"
+export { RobotoffQuestion } from "./components/robotoff/robotoff-question"
+export { RobotoffQuestionForm } from "./components/robotoff/robotoff-question-form"
 export { EnlargeIcon } from "./components/icons/zoom"
 export { ReduceIcon } from "./components/icons/unzoom"
 export { ZoomUnzoomButton } from "./components/buttons/zoom-unzoom-button"
