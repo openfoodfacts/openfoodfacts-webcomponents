7a4946164b959efe96219df3035fb640b3151fa5
Kout95 <antoine.bernier@outlook.fr>
2025-02-18 10:26:19 +0100

    fix: comment

--- web-components/src/utils/signals.ts
+++ web-components/src/utils/signals.ts
@@ -1,3 +1,4 @@
+// Set signal object state attribute
 // Do not use Object assign because it does not work with signals watchers
 export const setSignalObjectStateAttribute = (signal: any, attribute: string, value: any) => {
   signal.set({ ...signal.get(), [attribute]: value })
