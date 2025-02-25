4a3148ff1d0e6a904022dc9afcf5364cae6d8d6b
Kout95 <antoine.bernier@outlook.fr>
2025-02-25 15:24:16 +0100

    fix

--- web-components/src/components/robotoff-nutrients/robotoff-nutrients.ts
+++ web-components/src/components/robotoff-nutrients/robotoff-nutrients.ts
@@ -1,6 +1,6 @@
 import { Task } from "@lit/task"
-import { html, LitElement } from "lit"
-import { customElement, property } from "lit/decorators.js"
+import { css, html, LitElement, nothing } from "lit"
+import { customElement, property, state } from "lit/decorators.js"
 import {
   annotateNutrients,
   fetchIncompleteNutrientsInsightsByProductCode,
@@ -10,13 +10,36 @@ import "./robotoff-nutrients-table"
 import { fetchNutrientsTaxonomies } from "../../signals/taxonomies"
 import { InsightAnnotationAnswer } from "../../types/robotoff"
 import { BASE } from "../../styles/base"
+import { msg } from "@lit/localize"
 
 @customElement("robotoff-nutrients")
 export class RobotoffNutrients extends LitElement {
-  static override styles = [BASE]
+  static override styles = [
+    BASE,
+    css`
+      :host {
+        max-width: 500px;
+      }
+      .messages-wrapper {
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 400px;
+        text-align: center;
+      }
+    `,
+  ]
 
   @property({ type: String, attribute: "product-code" })
   productCode: string = ""
+
+  @property({ type: Boolean, attribute: "show-messages" })
+  showMessages: boolean = false
+
+  @state()
+  isSubmited: boolean = false
+
+  @state()
+  showSuccessMessage: boolean = false
 
   private _insightsTask = new Task(this, {
     task: async ([productCode], {}) => {
@@ -33,8 +56,28 @@ export class RobotoffNutrients extends LitElement {
     args: () => [this.productCode],
   })
 
-  onSubmit = (event: CustomEvent<InsightAnnotationAnswer>) => {
-    annotateNutrients(event.detail)
+  onSubmit = async (event: CustomEvent<InsightAnnotationAnswer>) => {
+    await annotateNutrients(event.detail)
+    this.isSubmited = true
+    this.showSuccessMessage = true
+    setTimeout(() => (this.showSuccessMessage = false), 3000)
+  }
+
+  renderMessages() {
+    if (!this.showMessages) {
+      return nothing
+    }
+
+    if (!this.isSubmited) {
+      return msg(
+        html`Openfoodfacts lives thanks to its community.<br />
+          Can you help us validate nutritional information?`
+      )
+    }
+    if (this.showSuccessMessage) {
+      return msg("Thank you for your help!")
+    }
+    return nothing
   }
 
   override render() {
@@ -45,6 +88,7 @@ export class RobotoffNutrients extends LitElement {
           return html`<p>No insights</p>`
         }
         return html`<div>
+          <p class="messages-wrapper"><i>${this.renderMessages()}</i></p>
           <robotoff-nutrients-table
             .insight="${insight}"
             @submit="${this.onSubmit}"
