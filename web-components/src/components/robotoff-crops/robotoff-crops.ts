31f6266e66173399d328daa5d5ea41ef937bf61a
Kout95 <antoine.bernier@outlook.fr>
2025-03-21 12:02:57 +0100

    feat: update crop

--- web-components/src/components/robotoff-crops/robotoff-crops.ts
+++ web-components/src/components/robotoff-crops/robotoff-crops.ts
@@ -1,5 +1,5 @@
-import { LitElement, html, nothing } from "lit"
-import { customElement, property } from "lit/decorators.js"
+import { LitElement, css, html, nothing } from "lit"
+import { customElement, property, state } from "lit/decorators.js"
 import "../shared/zoomable-image"
 import { ButtonType, getButtonClasses } from "../../styles/buttons"
 import robotoff from "../../api/robotoff"
@@ -12,11 +12,42 @@ import { Task } from "@lit/task"
 import { getRobotoffImageUrl } from "../../signals/robotoff"
 import { localized, msg } from "@lit/localize"
 import "../shared/loader"
+import { FLEX } from "../../styles/utils"
 
 @customElement("robotoff-crops")
 @localized()
 export class RobotoffCrops extends LitElement {
-  static override styles = [...getButtonClasses([ButtonType.Chocolate, ButtonType.Cappucino])]
+  static override styles = [
+    css`
+      .question {
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+        text-align: center;
+      }
+
+      .button-container {
+        display: flex;
+        justify-content: center;
+        gap: 0.5rem;
+        margin-top: 1.5rem;
+        margin-bottom: 0.5rem;
+      }
+
+      .crop-button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+      }
+    `,
+    FLEX,
+    ...getButtonClasses([
+      ButtonType.Chocolate,
+      ButtonType.Cappucino,
+      ButtonType.White,
+      ButtonType.LINK,
+    ]),
+  ]
 
   @property({ type: Number })
   count: number = 5
@@ -34,6 +65,9 @@ export class RobotoffCrops extends LitElement {
 
   @property({ type: Object })
   predictions: ImagePredictionsResponse | null = null
+
+  @state()
+  cropMode: boolean = false
 
   private _predictionTask = new Task(this, {
     task: async (
@@ -54,6 +88,43 @@ export class RobotoffCrops extends LitElement {
     },
     args: () => [this.count, this.page, this.barcode, this.modelName, this.minConfidence],
   })
+
+  renderCropAnswerButtons() {
+    if (this.cropMode) {
+      return html`
+        <div>
+          <div class="crop-button-container">
+            <button class="button cappucino-button" @click="${this.toggleCropMode}">
+              ${msg("Cancel crop")}
+            </button>
+          </div>
+        </div>
+      `
+    }
+    return html`
+      <div class="flex flex-col align-center">
+        <div class="button-container">
+          <button class="button cappucino-button" @click="${() => this.answer("1")}">
+            ${msg("Yes")}
+          </button>
+          <button class="button cappucino-button" @click="${() => this.answer("0")}">
+            ${msg("No")}
+          </button>
+          <button class="button white-button" @click="${() => this.answer("-1")}">
+            ${msg("Skip")}
+          </button>
+        </div>
+        <div>
+          <span>${msg("or")}</span>
+        </div>
+        <div class="crop-button-container">
+          <button class="button chocolate-button" @click="${this.toggleCropMode}">
+            ${msg("I'll propose a better crop")}
+          </button>
+        </div>
+      </div>
+    `
+  }
 
   override render() {
     return this._predictionTask.render({
@@ -76,20 +147,22 @@ export class RobotoffCrops extends LitElement {
     return html`
       <div>
         <div>
+          <p class="question">
+            <i>${msg("Is this picture a good view of the ingredients in the food?")}</i>
+          </p>
           <zoomable-image
             src=${imgUrl}
             .size="${{ width: "500px", height: "500px" }}"
-            crop-mode=${true}
+            ?crop-mode=${this.cropMode}
           ></zoomable-image>
-          <p>${msg("Is this image a crop?")}</p>
-          <div class="button-container">
-            <button @click="${() => this.answer("1")}">${msg("Yes")}</button>
-            <button @click="${() => this.answer("0")}">${msg("No")}</button>
-            <button @click="${() => this.answer("-1")}">${msg("Skip")}</button>
-          </div>
         </div>
+        ${this.renderCropAnswerButtons()}
       </div>
     `
+  }
+
+  toggleCropMode() {
+    this.cropMode = !this.cropMode
   }
 
   answer(value: QuestionAnnotationAnswer) {
