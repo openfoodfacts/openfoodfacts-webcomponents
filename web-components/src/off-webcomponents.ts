d9fa6e85d57b75df259bd93ee18514200b413ec7
ekas-7 <ekaspreetatwal@gmail.com>
2025-03-22 14:35:57 +0530

    corrected linting error 5

--- web-components/src/off-webcomponents.ts
+++ web-components/src/off-webcomponents.ts
@@ -1,9 +1,5 @@
-
-
-
-
 export { setLocale, getLocale } from "./localization"
 export { OffWebcomponentsConfiguration as OffWebcomponentConfiguration } from "./components/shared/off-webcomponents-configuration"
 export { RobotoffQuestion } from "./components/robotoff-question/robotoff-question"
 export { RobotoffNutrients } from "./components/robotoff-nutrients/robotoff-nutrients"
-export {KnowledgePanelComponent} from "./components/knowledge-panels/knowledge-panels"
+export { KnowledgePanelComponent } from "./components/knowledge-panels/knowledge-panels"
