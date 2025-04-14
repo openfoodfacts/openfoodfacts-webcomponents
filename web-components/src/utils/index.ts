cea04f336f4d70caaba374bb3cf46df18da14f0f
Kout95 <antoine.bernier@outlook.fr>
2025-04-14 16:09:55 +0200

    fix

--- web-components/src/utils/index.ts
+++ web-components/src/utils/index.ts
@@ -1,57 +1,6 @@
 import DOMPurify from "dompurify"
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js"
 
-const reformatTagMapping = {
-  " ": "-",
-  "'": "-",
-  "&": "",
-  à: "a",
-  â: "a",
-  ä: "a",
-  é: "e",
-  è: "e",
-  ê: "e",
-  ë: "e",
-  î: "i",
-  ï: "i",
-  ô: "o",
-  ö: "o",
-  û: "u",
-  ù: "u",
-  ü: "u",
-}
-
-export const reformatValueTag = (value: string) => {
-  if (!value) {
-    return value
-  }
-  let output = value.trim().toLowerCase()
-  for (const [search, replace] of Object.entries(reformatTagMapping)) {
-    output = output.replace(new RegExp(search, "g"), replace)
-  }
-  output = output.replace(/-{2,}/g, "-")
-  return output
-}
-
-export const removeEmptyKeys = (obj: Record<string, any>) => {
-  Object.keys(obj).forEach((key) => (obj[key] == null || obj[key] === "") && delete obj[key])
-  return obj
-}
-
-//  Only for testing purpose
-export function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-//to provide capitalised country name; en:france => France
-export const capitaliseName = (string: string | undefined) => {
-  if (!string) {
-    return string
-  }
-  const name = string.slice(3)
-  return name.charAt(0).toUpperCase() + name.slice(1)
-}
-
 export const paramToString = (value: any): string => {
   if (isNullOrUndefined(value)) {
     return ""
