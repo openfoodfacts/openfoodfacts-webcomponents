5ee085e825c2ccc194a6ee00d99d0090accdcd1c
Kout95 <antoine.bernier@outlook.fr>
2025-04-14 18:47:25 +0200

    fix

--- web-components/src/utils/html.ts
+++ web-components/src/utils/html.ts
@@ -1,6 +1,6 @@
 // Function to sanitize HTML and return it as unsafeHTML for Lit rendering
 
-import { unsafeHTML } from "lit-html/directives/unsafe-html"
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js"
 import DOMPurify from "dompurify"
 
 // It allow to dynamically create HTML elements with a specific tag name and be sure it's safe
