575ec5e722e1c08aeef0bd37b84069559737c11d
Kout95 <antoine.bernier@outlook.fr>
2025-04-24 12:42:48 +0200

    chore

--- web-components/src/signals/openfoodfacts.ts
+++ web-components/src/signals/openfoodfacts.ts
@@ -1,4 +1,6 @@
 import { signal } from "@lit-labs/signals"
 
 export const BASE_URL = "https://world.openfoodfacts.org/api/v2"
+
+// this enable changing open food facts URL globally
 export const openfoodfactsApiUrl = signal(BASE_URL)
