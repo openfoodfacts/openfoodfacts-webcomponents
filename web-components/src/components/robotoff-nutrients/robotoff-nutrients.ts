1536e1069554cb572bcb3ac97bb9da3e15d5bb1d
Kout95 <antoine.bernier@outlook.fr>
2025-05-12 10:22:03 +0200

    fix

--- web-components/src/components/robotoff-nutrients/robotoff-nutrients.ts
+++ web-components/src/components/robotoff-nutrients/robotoff-nutrients.ts
@@ -59,12 +59,22 @@ export class RobotoffNutrients extends LitElement {
         width: 100%;
       }
 
+      .nutrients {
+        container-type: inline-size;
+      }
       .nutrients-content-wrapper {
         position: relative;
         display: flex;
-        flex-wrap: wrap;
         align-items: start;
         gap: 0.5rem 5rem;
+      }
+
+      @container (max-width: ${Breakpoints.MD}px) {
+        .nutrients-content-wrapper {
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
       }
     `,
   ]
