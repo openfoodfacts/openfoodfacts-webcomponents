79fc260b1632daab96c65250157c3d4d72137624
heymitali <mitalisinghnaruka@gmail.com>
2025-07-18 09:51:02 +0530

    cursor-pointer css added

--- web-components/src/components/product-card/product-card.ts
+++ web-components/src/components/product-card/product-card.ts
@@ -45,6 +45,7 @@ export class ProductCard extends LitElement {
       box-shadow:
         0 4px 6px -1px rgba(0, 0, 0, 0.1),
         0 2px 4px -1px rgba(0, 0, 0, 0.06);
+      cursor: pointer;
     }
 
     .dark-mode {
