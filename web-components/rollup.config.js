78683cd2c0f727f9b971f3dc40795110bdd9d9c2
VaiTon <eyadlorenzo@gmail.com>
2025-09-04 15:24:09 +0200

    chore: do not minify bundled JS, fix dynamic imports

--- web-components/rollup.config.js
+++ web-components/rollup.config.js
@@ -5,12 +5,13 @@
  */
 
 import summary from "rollup-plugin-summary"
-import terser from "@rollup/plugin-terser"
-import resolve from "@rollup/plugin-node-resolve"
+import nodeResolve from "@rollup/plugin-node-resolve"
 import replace from "@rollup/plugin-replace"
 import dotenv from "dotenv"
 import typescript from "@rollup/plugin-typescript"
 import copy from "rollup-plugin-copy"
+import dynamicImportVariables from "@rollup/plugin-dynamic-import-vars"
+import terser from "@rollup/plugin-terser"
 
 // Load environment variables from .env file
 dotenv.config()
@@ -18,37 +19,36 @@ dotenv.config()
 const production = process.env.MODE !== "dev"
 console.log("IS PRODUCTION", production)
 
-let output = []
-if (production) {
-  output.push(
-    {
-      file: "dist/off-webcomponents.bundled.js",
-      format: "esm",
-      sourcemap: true,
-    }
-    // Remove because language is not supported in commonjs
-    // {
-    //   file: "dist/off-webcomponents.bundled.cjs",
-    //   format: "cjs",
-    //   sourcemap: true,
-    // }
-  )
-} else {
-  output.push({
-    file: "dist/off-webcomponents.js",
-    format: "esm",
-    sourcemap: true,
-  })
-}
-
 const elementsToCopy = [
   { src: "src/assets", dest: "dist" }, // This will copy src/assets to dist/assets
   !production && { src: "index.html", dest: "dist" }, // This will copy src/index.html to dist/index.html in development mode
 ].filter(Boolean)
 
+/** @type {import('rollup').RollupOptions} */
 export default {
   input: "src/off-webcomponents.ts",
-  output,
+  output: [
+    {
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      format: "cjs",
+      entryFileNames: "[name].cjs",
+      chunkFileNames: "[name]-[hash].cjs",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
+      entryFileNames: "[name].min.js",
+      chunkFileNames: "[name]-[hash].min.js",
+      plugins: [terser()],
+    },
+  ],
   onwarn(warning) {
     if (warning.code !== "THIS_IS_UNDEFINED") {
       console.error(`(!) ${warning.message}`)
@@ -59,7 +59,10 @@ export default {
       tsconfig: "tsconfig.json",
     }),
     replace({ preventAssignment: false, "Reflect.decorate": "undefined" }),
-    resolve(),
+    dynamicImportVariables({
+      errorWhenNoFilesFound: true,
+    }),
+    nodeResolve(),
 
     replace({
       preventAssignment: true,
@@ -77,24 +80,9 @@ export default {
     //   }),
     // !production && livereload("dist"),
 
-    /**
-     * This minification setup serves the static site generation.
-     * For bundling and minification, check the README.md file.
-     */
-    production &&
-      terser({
-        ecma: 2021,
-        module: true,
-        warnings: true,
-        mangle: {
-          properties: {
-            regex: /^__/,
-          },
-        },
-      }),
-    summary(),
     copy({
       targets: elementsToCopy,
     }),
+    summary(),
   ],
 }
