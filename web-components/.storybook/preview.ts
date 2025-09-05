f84679890b8938525966029e271f8c2374185d4a
VaiTon <eyadlorenzo@gmail.com>
2025-09-05 22:43:02 +0200

    fix: update assetsImagesPath to use BASE_URL for image assets

--- web-components/.storybook/preview.ts
+++ web-components/.storybook/preview.ts
@@ -5,6 +5,10 @@ import { setCustomElementsManifest } from "@storybook/web-components-vite"
 import customElements from "../custom-elements.json"
 setCustomElementsManifest(customElements)
 
+import { assetsImagesPath } from "../src/signals/app"
+
+assetsImagesPath.set(import.meta.env.BASE_URL + "assets/images")
+
 const preview: Preview = {
   parameters: {
     controls: {
