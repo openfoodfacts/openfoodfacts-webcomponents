6f739788dc6f4550972b92ae852b5f9e9ee25e77
VaiTon <eyadlorenzo@gmail.com>
2025-09-06 12:02:31 +0200

    chore: add sourcemaps and manual chunking for localization files in Vite configuration

--- web-components/vite.config.ts
+++ web-components/vite.config.ts
@@ -6,12 +6,25 @@ import { viteStaticCopy } from "vite-plugin-static-copy"
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+const languagesRegex = /src\/localization\/dist\/locales\/(.*)\.ts$/
+
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/off-webcomponents.ts"),
       fileName: "off-webcomponents",
       formats: ["es", "cjs"],
+    },
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          const match = id.match(languagesRegex)
+          if (match) {
+            return `lang/${match[1]}`
+          }
+        },
+      },
     },
   },
   plugins: [
