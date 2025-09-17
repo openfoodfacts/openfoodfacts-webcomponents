dd41d1dbad2089a0ed5b5a91d826a3efc8f1b755
VaiTon <eyadlorenzo@gmail.com>
2025-09-18 00:31:38 +0200

    chore: Add TODO to move NutrientTaxonomy type to SDK

--- web-components/src/types/taxonomies.ts
+++ web-components/src/types/taxonomies.ts
@@ -1,4 +1,5 @@
 import type { Nutrient } from "@openfoodfacts/openfoodfacts-nodejs"
 
+// TODO: move this type to the SDK
 export type NutrientTaxonomy = Nutrient & { id: string }
 export type NutrientsTaxonomiesResponse = Record<string, NutrientTaxonomy>
