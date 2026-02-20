e36dba9bcdc87d704515f297def4e84746280a22
Ajith <ajithpkumar231@gmail.com>
2026-02-21 00:40:57 +0530

    fix-419 Fixed dark mode issue, View Product option not visible in Nutrition, Ingredients/detection,spell-check

--- web-components/src/components/shared/product-link-button.ts
+++ web-components/src/components/shared/product-link-button.ts
@@ -4,6 +4,8 @@ import { msg } from "@lit/localize"
 import { openfoodfactsApiUrl } from "../../signals/openfoodfacts"
 import { ButtonType, getButtonClasses } from "../../styles/buttons"
 import "../icons/external-link"
+import { darkModeListener } from "../../utils/dark-mode-listener"
+import { classMap } from "lit/directives/class-map.js"
 
 /**
  * A button component that displays as a link with a link icon.
@@ -11,11 +13,37 @@ import "../icons/external-link"
  */
 @customElement("product-link-button")
 export class ProductLinkButton extends LitElement {
+
+  isDarkMode = darkModeListener.darkMode
+
+  private _darkModeCb = (isDark: boolean) => {
+    this.isDarkMode = isDark
+    this.requestUpdate()
+  }
+
+  override connectedCallback() {
+    super.connectedCallback()
+    darkModeListener.subscribe(this._darkModeCb)
+  }
+
+  override disconnectedCallback() {
+    darkModeListener.unsubscribe(this._darkModeCb)
+    super.disconnectedCallback()
+  }
   static override styles = [
     getButtonClasses([ButtonType.LINK]),
     css`
       :host {
         display: inline-block;
+      }
+      .dark-mode {
+        color: #f9f7f5;
+      }
+
+      .dark-mode h2,
+      .dark-mode p,
+      .dark-mode button{
+        color: #f9f7f5;
       }
     `,
   ]
@@ -35,14 +63,16 @@ export class ProductLinkButton extends LitElement {
       console.error("Product code is required")
       return nothing
     }
-
+    const rootClasses = { "dark-mode": this.isDarkMode }
     return html`
+     <section class=${classMap(rootClasses)}> 
       <a href="${this.productUrl}" target="_blank" rel="noopener noreferrer">
         <button class="link-button button with-icon small">
           <external-link-icon></external-link-icon>
           <span>${msg("View Product")}</span>
         </button>
       </a>
+    </section>
     `
   }
 }
