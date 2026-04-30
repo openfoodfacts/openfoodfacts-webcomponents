0713fefaa0b03190cd760d309b58d6d635cf0cb5
Shivani <shivaninayak386@gmail.com>
2026-04-30 23:39:13 +0530

    fix: remove invalid git prefix from import

--- web-components/src/components/robotoff-nutrient-extraction/robotoff-nutrient-extraction.ts
+++ web-components/src/components/robotoff-nutrient-extraction/robotoff-nutrient-extraction.ts
@@ -1,4 +1,4 @@
-git import { Task } from "@lit/task"
+import { Task } from "@lit/task"
 import { css, html, LitElement, nothing } from "lit"
 import { customElement, property, state } from "lit/decorators.js"
 import {
