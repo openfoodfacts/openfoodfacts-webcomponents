b33842e86bfc662c4f5ca5b89553495772ad8e81
SRIJAN  SARKAR <srijansarkar2007@gmail.com>
2026-07-09 21:45:00 +0530

    Refactor community links component
    Removed unused SocialPlatform interface and SVG icons for social platforms. Updated the platforms array to include only relevant social media links.

--- web-components/src/components/community-links/community-links.ts
+++ web-components/src/components/community-links/community-links.ts
@@ -8,52 +8,6 @@ import type { TemplateResult } from "lit"
 
 
 
-interface SocialPlatform {
-  name: string
-  imageUrl?: string
-  inlineSvg?: TemplateResult
-  url: string
-  ariaLabel: string
-}
-
-// Inline SVG icons for platforms that need crisp, reliable rendering
-const newsletterIcon = svg`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <rect x="2" y="4" width="20" height="16" rx="2.5" fill="#c0392b"/>
-    <path d="M2 7.5l10 6.5 10-6.5" stroke="#fff" stroke-width="1.5" fill="none" stroke-linecap="round"/>
-    <rect x="2" y="4" width="20" height="16" rx="2.5" fill="none" stroke="#a93226" stroke-width="0.5"/>
-  </svg>
-`
-
-const forumIcon = svg`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <!-- Discourse-style stacked speech bubbles -->
-    <circle cx="9" cy="10" r="7" fill="#00aeef"/>
-    <path d="M9 17c0 3.5-4 5-4 5s9.5-.5 11-5c.5-1.5 0-3-.5-4" fill="#0090c9"/>
-    <circle cx="6.5" cy="10" r="1" fill="#fff"/>
-    <circle cx="9.5" cy="10" r="1" fill="#fff"/>
-    <circle cx="12.5" cy="10" r="1" fill="#fff"/>
-  </svg>
-`
-
-const xTwitterIcon = svg`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <rect width="24" height="24" rx="4" fill="#000"/>
-    <path d="M17.53 4h2.23L14.23 10.52 20.75 20h-5.37l-3.9-5.1L7.13 20H4.9l6.02-6.88L4 4h5.48l3.53 4.61L17.53 4zm-.78 14.4h1.23L7.36 5.27H6.03l10.72 13.13z" fill="#fff"/>
-  </svg>
-`
-
-const platforms: SocialPlatform[] = [
-  { name: "Newsletter", inlineSvg: newsletterIcon, url: "https://world.openfoodfacts.org/newsletter", ariaLabel: "Subscribe to our Newsletter" },
-  { name: "Forum", inlineSvg: forumIcon, url: "https://forum.openfoodfacts.org", ariaLabel: "Visit the Open Food Facts Forum" },
-  { name: "X", inlineSvg: xTwitterIcon, url: "https://x.com/OpenFoodFacts", ariaLabel: "Follow us on X (Twitter)" },
-  { name: "Instagram", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png", url: "https://www.instagram.com/open.food.facts", ariaLabel: "Follow us on Instagram" },
-  { name: "Facebook", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg", url: "https://www.facebook.com/OpenFoodFacts", ariaLabel: "Follow us on Facebook" },
-  { name: "GitHub", imageUrl: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", url: "https://github.com/openfoodfacts", ariaLabel: "Visit our GitHub" },
-  { name: "Mastodon", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/48/Mastodon_Logotype_%28Simple%29.svg", url: "https://mastodon.social/@openfoodfacts", ariaLabel: "Follow us on Mastodon" },
-  { name: "Bluesky", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg", url: "https://bsky.app/profile/openfoodfacts.org", ariaLabel: "Follow us on Bluesky" },
-  { name: "Slack", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg", url: "https://slack.openfoodfacts.org", ariaLabel: "Join our Slack" }
-]
 
 /**
  * @element community-links
