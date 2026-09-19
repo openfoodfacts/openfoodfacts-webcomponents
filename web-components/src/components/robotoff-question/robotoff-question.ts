f5d09326d49ea33491b49ce760ff26a97e5f4616
VaiTon <eyadlorenzo@gmail.com>
2026-09-19 15:57:13 +0200

    style: apply updated Prettier formatting

--- web-components/src/components/robotoff-question/robotoff-question.ts
+++ web-components/src/components/robotoff-question/robotoff-question.ts
@@ -151,15 +151,17 @@ export class RobotoffQuestion extends SignalWatcher(LitElement) {
         return html`
           <div class="question-wrapper">
             ${this.renderMessage()}
-            ${isQuestionsFinished(this.productCode).get()
-              ? nothing
-              : html`
-                  <robotoff-question-form
-                    .isImageExpanded=${this.isImageExpanded}
-                    .question=${question}
-                    @submit=${this.onQuestionAnswered}
-                  ></robotoff-question-form>
-                `}
+            ${
+              isQuestionsFinished(this.productCode).get()
+                ? nothing
+                : html`
+                    <robotoff-question-form
+                      .isImageExpanded=${this.isImageExpanded}
+                      .question=${question}
+                      @submit=${this.onQuestionAnswered}
+                    ></robotoff-question-form>
+                  `
+            }
           </div>
         `
       },
